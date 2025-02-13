981aba4f22c06b7b84705a8088b8ce288b76fc6f
Vanessa Do <xex.vanessa@gmail.com>
2025-02-12 21:10:35 -0500

    Task 2- Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -5,3 +5,11 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 }
 console.log(`Net Salary: $${calculateSalary(5000, 500, 0.1)}`); // Expected output: "Net Salary: $5000.00"
 console.log(`Net Salary: $${calculateSalary(7000, 1000, 0.15)}`); // Expected output: "Net Salary: $6950.00"
+
+// Task 2: Functions Expression 
+// declared a function to get discounted prices 
+const calculateDiscount = (price, discountRate) => {
+    return price - (price * discountRate)
+}
+console.log(`Final Price: $${calculateDiscount(100, 0.2)}`); // Expected output: "Final Price: $80.00"
+console.log(`Final Price: $${calculateDiscount(250, 0.15)}`); // Expected output: "Final Price: $212.50"
