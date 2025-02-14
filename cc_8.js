74a2128eee5e0b4f8aa434ccd4e2b5911d15edc4
Vanessa Do <xex.vanessa@gmail.com>
2025-02-13 21:51:53 -0500

    Edited Task 1 and 2

--- cc_8.js
+++ cc_8.js
@@ -1,7 +1,8 @@
 // Task 1: Function Declaration
 // created a function to calculate employee salaries 
 function calculateSalary(baseSalary, bonus, taxRate) {
-    return (baseSalary + bonus) - (baseSalary * taxRate)
+    salary = (baseSalary + bonus) - (baseSalary * taxRate)
+    return salary.toFixed(2);
 }
 console.log(`Net Salary: $${calculateSalary(5000, 500, 0.1)}`); // Expected output: "Net Salary: $5000.00"
 console.log(`Net Salary: $${calculateSalary(7000, 1000, 0.15)}`); // Expected output: "Net Salary: $6950.00"
@@ -9,7 +10,8 @@ console.log(`Net Salary: $${calculateSalary(7000, 1000, 0.15)}`); // Expected ou
 // Task 2: Functions Expression 
 // declared a function to get discounted prices 
 const calculateDiscount = (price, discountRate) => {
-    return price - (price * discountRate)
+    discountPrice = price - (price * discountRate)
+    return discountPrice.toFixed(2);
 }
 console.log(`Final Price: $${calculateDiscount(100, 0.2)}`); // Expected output: "Final Price: $80.00"
 console.log(`Final Price: $${calculateDiscount(250, 0.15)}`); // Expected output: "Final Price: $212.50"
